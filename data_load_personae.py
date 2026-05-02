bba26009fb177c0c41838476cf3535e7acb15a37
OscarBuzz2006 <sworrub.racso@gmail.com>
2026-05-02 17:47:59 +1000

    Commit message.

---
+++ data_load_personae.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+df_molts = pd.read_csv("hf://datasets/ronantakizawa/moltbook/moltbook_posts.csv")
+df_submolts = pd.read_csv("hf://datasets/ronantakizawa/moltbook/moltbook_submolts.csv")
+
